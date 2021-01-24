e9059209382065b1346237aea4828220c888fa56
AM <73906893+amhappylearning@users.noreply.github.com>
2021-01-25 00:47:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+var first_name = "ABC";
+var last_name = "XYZ";
+
+console.log(first_name);
+
+console.log(last_name);
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var pow = math.pow(4, 3)
+console.log(pow);
+
+var minimum_number = Math.min(100, 70);
+console.log(minimum_number);
+
+
+var maximum_number = Math.min(100, 70);
+console.log(maximum_number);
+
+var sqrt = math.sqrt(25);
+console.log(sqrt);
+
+var name1 = "abcd";
+var uppercase = name1.toUpperCase();
+console.log(uppercase);
+
+var name2 = "ABCD";
+var lowercase = name2.toLowerCase();
+console.log(lowercase);
+
+var replace_name = name1.replace("abcd", "abcdef");
